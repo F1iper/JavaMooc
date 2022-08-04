7a82018ca674c42397040e37570459b80e69598a
F1iper <ftimofiejew@gmail.com>
2022-08-04 09:58:04 +0200

    Part 4, Excercise 20 Implementation

---
+++ src/main/java/org/exercise/part4/books20/Book.java
@@ -0,0 +1,43 @@
+package org.exercise.part4.books20;
+
+public class Book {
+    
+    private String title;
+    private int pages;
+    private int publicationYear;
+    
+    @Override
+    public String toString() {
+        return title + ", " + pages + " pages, " + publicationYear;
+    }
+    
+    Book(final String title, final int pages, final int publicationYear) {
+        this.title = title;
+        this.pages = pages;
+        this.publicationYear = publicationYear;
+    }
+    
+    String getTitle() {
+        return title;
+    }
+    
+    void setTitle(final String title) {
+        this.title = title;
+    }
+    
+    int getPages() {
+        return pages;
+    }
+    
+    void setPages(final int pages) {
+        this.pages = pages;
+    }
+    
+    int getPublicationYear() {
+        return publicationYear;
+    }
+    
+    void setPublicationYear(final int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+}
---
+++ src/main/java/org/exercise/part4/books20/Main.java
@@ -0,0 +1,42 @@
+package org.exercise.part4.books20;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        List<Book> books = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            System.out.println("Enter the title: ");
+            String title = scanner.nextLine();
+            
+            if (title.isEmpty()) {
+                break;
+            }
+            
+            System.out.println("Enter number of pages: ");
+            int pages = Integer.valueOf(scanner.nextLine());
+            
+            System.out.println("Enter publication year: ");
+            int pubYear = Integer.valueOf(scanner.nextLine());
+            books.add(new Book(title, pages, pubYear));
+        }
+        
+        System.out.println("What iformation will be printed?");
+        String input = scanner.nextLine();
+        
+        if (input.equals("everything")) {
+            for (Book book : books) {
+                System.out.println(book);
+            }
+        } else if (input.equals("name")) {
+            for (Book book : books) {
+                System.out.println(book.getTitle());
+            }
+        }
+    }
+}
