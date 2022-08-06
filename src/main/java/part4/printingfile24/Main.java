4fb95106753dc1b9a5a4474dfe18cf4ed9dd5c31
F1iper <ftimofiejew@gmail.com>
2022-08-06 11:33:02 +0200

    Reading file with empty lines

--- src/main/java/part4/printingfile24/Main.java
+++ src/main/java/part4/printingfile24/Main.java
@@ -6,11 +6,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         
-        try (Scanner scanner = new Scanner(Paths.get("src/file.txt"))) {
+        try (Scanner scanner = new Scanner(Paths.get("file.txt"))) {
             
             while (scanner.hasNext()) {
                 String row = scanner.nextLine();
                 
+                if (row.isEmpty()) {
+                    continue;
+                }
                 System.out.println(row);
             }
             
